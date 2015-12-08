0139494430372292ccef319b8c68d968a10df49f
khansaamia <khansaamia@yahoo.com>
2015-12-07 22:53:44 -0800

    Create WealthManager.java

---
+++ src/csm10jHw03/WealthManager.java
@@ -0,0 +1,12 @@
+
+package javagui;
+
+public class WealthManager {
+
+    public static void main(String[] args) {
+        
+        WealthManagerForm wmf = new WealthManagerForm();
+        
+    }
+    
+}
