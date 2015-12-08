8049cfe101f8e29e1dd4512e2017b77eb567c03d
khansaamia <khansaamia@yahoo.com>
2015-12-07 22:55:25 -0800

    Create Car.java

---
+++ src/csm10jHw03/WealthManagement/Car.java
@@ -0,0 +1,9 @@
+
+package WealthManagement;
+
+public class Car extends Property{
+    
+    public Car(String name, double propertyValue, double debtAmount){
+        super(name, propertyValue, debtAmount);
+    }
+}
