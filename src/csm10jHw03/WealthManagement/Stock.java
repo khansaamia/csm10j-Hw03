5e780ae68c37f0246b002d3129db531e6c76720c
khansaamia <khansaamia@yahoo.com>
2015-12-07 22:56:47 -0800

    Create Stock.java

---
+++ src/csm10jHw03/WealthManagement/Stock.java
@@ -0,0 +1,10 @@
+
+package WealthManagement;
+
+public class Stock extends Security {
+    
+    public Stock(String name, int quantity, double price){
+        super(name,quantity,price);
+    }
+    
+}
